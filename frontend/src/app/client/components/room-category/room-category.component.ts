64c5b3d064e1e6f95efc63aeb3f0cfe4bed6f016
delfin <delfinjfb@gmail.com>
2025-01-21 21:04:19 +0100

    feat: add RoomCategoryComponent with template and tests

---
+++ frontend/src/app/client/components/room-category/room-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-category',
+  imports: [],
+  templateUrl: './room-category.component.html',
+  styleUrl: './room-category.component.scss'
+})
+export class RoomCategoryComponent {
+
+}
